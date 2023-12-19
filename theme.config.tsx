01da5677c5dac544f8ce5ed0562a93bc437c1806
jaredliu <jaredliu233@gmail.com>
2023-12-19 11:48:08 +0800

    chore: init

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+    logo: (
+      <Image src="/images/logo_with_text.png" alt="" width={128.8} height={42}></Image>
+    ),
+    // project: {
+    //     link: "https://github.com/shuding/nextra-docs-template",
+    // },
+    // chat: {
+    //     link: "https://discord.com",
+    // },
+    docsRepositoryBase: "https://github.com/DophinL/brieflow-help",
+    footer: {
+        text: "Build with ❤️",
+    },
+};
 
-export default config
+export default config;
